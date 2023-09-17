b233c188b7b4bb680414a35bcad575d451ac0188
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 18:09:56 +1000

    Update Player.py
    player can move

--- Player.py
+++ Player.py
@@ -2,8 +2,14 @@ import pygame
 
 class Player():
     def __init__(self) -> None:
-        self.position = pygame.Vector2(100, 100)
+        self.position = pygame.Vector2(10, -100)
         self.velocity = pygame.Vector2(0, 5)
         self.size = pygame.Vector2(30, 30)
     def physicsProcess(self):
+        self.velocity.x = 0
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            self.velocity.x -= 5
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            self.velocity.x += 5
+        
         self.position += self.velocity
