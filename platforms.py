7410d417683763d44344e0a600e85c04f8592820
Reacto <122259384+reactoimpact@users.noreply.github.com>
2023-09-17 20:27:35 +1000

    Update platforms.py
    Added y camera offset

--- platforms.py
+++ platforms.py
@@ -2,5 +2,5 @@ import pygame
 
 class Platform:
     def __init__(self, x, y, width, height, camera, screen):
-        self.rect = pygame.Rect(x - camera.target.x + camera.offset.x, y, width, height)
+        self.rect = pygame.Rect(x - camera.target.x + camera.offset.x, y - camera.target.y + camera.offset.y, width, height)
         pygame.draw.rect(screen, (0,0,0), self.rect)
