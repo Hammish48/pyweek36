8a5b26c59f8d2038b372525f8b260fdfbd3b3e1c
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 10:25:40 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+pygame.init()
+
+def main():
+    screen = pygame.display.set_mode((1120, 580))
+    pygame.display.set_caption("caption")
+    fps = pygame.time.Clock()
+    pygame.display.set_icon(pygame.image.load("./assets/dcpfp.png"))
+    pen = pygame.draw
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill((0, 0, 0))
+
+        pen.rect(screen, (0, 200, 20), pygame.Rect(50, 50, 50,50))
+
+        fps.tick(60)
+        pygame.display.update()
+    
+
+if __name__ == "__main__":
+    main()
