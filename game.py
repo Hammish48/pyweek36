eb6a503cb6df6e46380c93ec3498bcc2c4911773
Reacto <122259384+reactoimpact@users.noreply.github.com>
2023-09-17 20:21:02 +1000

    Update game.py

--- game.py
+++ game.py
@@ -2,16 +2,22 @@ import pygame
 import sys
 from Player import Player
 from Camera import Camera
+from platforms import Platform
+
+
+
+
+
 
 class Game:
-    width = 1120
-    height = 580
-    
     def __init__(self) -> None:
         self.player = Player()
         self.camera = Camera(self.player.position, pygame.Vector2(560 - self.player.size.x/2, 290 - self.player.size.y/2))
 
     def run(self,screen, fps):
+        width = 1120
+        height = 580
+        
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -20,21 +26,28 @@ class Game:
             # game logic
             self.player.physicsProcess()
             # rendering
-            screen.fill((255, 255, 255))
+            screen.fill((0, 255, 10))
+
+
             
+
             player = pygame.Rect(self.player.position.x - self.camera.target.x + self.camera.offset.x, self.player.position.y - self.camera.target.y + self.camera.offset.y, 50,50)
-            platform = pygame.Rect(0 - self.camera.target.x + self.camera.offset.x, height - 100 - self.camera.target.y + self.camera.offset.y, width, height)
+            # platform = pygame.Rect(0 - self.camera.target.x + self.camera.offset.x, height - 100 - self.camera.target.y + self.camera.offset.y, width, height)
 
             pygame.draw.rect(screen, (0, 200, 20), player)
-            pygame.draw.rect(screen, (0,0,0), platform)
+
+            p_1 = Platform(0, height-100, width, 100, self.camera, screen)
+            # pygame.draw.rect(screen, (0,0,0), p_1.rect)
 
 
-            # check if player has collided with platform, if so set velocity & acceleration to 0 and move up 1 pixel to stop collide
-            if player.colliderect(platform):
+            
+            if player.colliderect(p_1.rect):
                 print("collide")
                 self.player.velocity = pygame.Vector2(0, 0)
                 self.player.acceleration = pygame.Vector2(0, 0)
                 self.player.position -= pygame.Vector2(0, 1)
+            
+
 
             fps.tick(60)
             pygame.display.update()
