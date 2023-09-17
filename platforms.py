bc96d79ec3cdee66433b1411e4fad7c15ed5d243
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 20:37:09 +1000

    Update platforms.py

--- platforms.py
+++ platforms.py
@@ -2,5 +2,9 @@ import pygame
 
 class Platform:
     def __init__(self, x, y, width, height, camera, screen):
-        self.rect = pygame.Rect(x - camera.target.x + camera.offset.x, y - camera.target.y + camera.offset.y, width, height)
+        self.position = pygame.Vector2(x, y)
+        self.size = pygame.Vector2(width, height)
+    def render(self,camera, screen):
+        self.rect = pygame.Rect(self.position.x - camera.target.x + camera.offset.x, self.position.y - camera.target.y + camera.offset.y, width, height)
         pygame.draw.rect(screen, (0,0,0), self.rect)
+        
