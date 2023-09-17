de5c5845a031cf7bd55ea7948ddb3c57538ef90d
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 17:54:36 +1000

    Update Player.py
    made player smol

--- Player.py
+++ Player.py
@@ -4,6 +4,6 @@ class Player():
     def __init__(self) -> None:
         self.position = pygame.Vector2(100, 100)
         self.velocity = pygame.Vector2(0, 5)
-        self.size = pygame.Vector2(50, 50)
+        self.size = pygame.Vector2(30, 30)
     def physicsProcess(self):
         self.position += self.velocity
