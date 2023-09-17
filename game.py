ee30e8c7651759e9eebc6ced3901c20b6ed0fa97
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 21:38:07 +1000

    Update game.py

--- game.py
+++ game.py
@@ -2,40 +2,32 @@ import pygame
 import sys
 from Player import Player
 from Camera import Camera
-from platforms import Platform
+from Block import Platform
+
+
 
 class Game:
     def __init__(self) -> None:
         self.player = Player()
         self.camera = Camera(self.player.position, pygame.Vector2(560 - self.player.size.x/2, 290 - self.player.size.y/2))
-        self.platforms = [Platform(50, 100, 100, 50)]
+        self.platforms = [Platform(0, 100, 100, 50), Platform(300, 100, 100, 50)]
     def run(self,screen, fps):
-        width = 1120
-        height = 580
-
+        
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             # game logic
-            self.player.physicsProcess()
+            self.player.physicsProcess(self.platforms)
             # rendering
             screen.fill((255, 255, 255))
 
-            player = pygame.Rect(self.player.position.x - self.camera.target.x + self.camera.offset.x, self.player.position.y - self.camera.target.y + self.camera.offset.y, 50,50)
-            pygame.draw.rect(screen, (0, 200, 20), player)
-
-
+            # platform = pygame.Rect(0 - self.camera.target.x + self.camera.offset.x, height - 100 - self.camera.target.y + self.camera.offset.y, width, height)
+            pygame.draw.rect(screen, (0, 200, 20), pygame.Rect(self.player.position.x - self.camera.target.x + self.camera.offset.x, self.player.position.y - self.camera.target.y + self.camera.offset.y, self.player.size.x, self.player.size.y))
+            pygame.draw.rect(screen, (255, 7, 156), pygame.Rect(self.player.position.x- self.camera.target.x + self.camera.offset.x, self.player.position.y - self.camera.target.y + self.camera.offset.y + self.player.size.y, self.player.size.x, 1))
             for platform in self.platforms:
                 platform.render(self.camera, screen)
-                if player.colliderect(platform.rect):
-                    print("collide")
-                    if abs(platform.rect.top - player.bottom) < 10:
-                        self.player.velocity = pygame.Vector2(0, 0)
-                        self.player.acceleration = pygame.Vector2(0, 0)
-            
-
 
             fps.tick(60)
             pygame.display.update()
