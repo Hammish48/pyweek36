f249dec4b0c23e3ca61ecc6a1dda92b17d0e9ac8
Reactoimpact <reactoimpact@hotmail.com>
2023-09-17 20:56:28 +1000

    minor bugs

--- game.py
+++ game.py
@@ -10,6 +10,8 @@ class Game:
         self.camera = Camera(self.player.position, pygame.Vector2(560 - self.player.size.x/2, 290 - self.player.size.y/2))
         self.platforms = [Platform(100, 100, 100, 50)]
     def run(self,screen, fps):
+        width = 1120
+        height = 580
 
         while True:
             for event in pygame.event.get():
@@ -22,15 +24,10 @@ class Game:
             screen.fill((255, 255, 255))
 
             player = pygame.Rect(self.player.position.x - self.camera.target.x + self.camera.offset.x, self.player.position.y - self.camera.target.y + self.camera.offset.y, 50,50)
-            # platform = pygame.Rect(0 - self.camera.target.x + self.camera.offset.x, height - 100 - self.camera.target.y + self.camera.offset.y, width, height)
-
             pygame.draw.rect(screen, (0, 200, 20), player)
 
-            p_1 = Platform(0, height-100, width, 100, self.camera, screen)
-            # pygame.draw.rect(screen, (0,0,0), p_1.rect)
 
-
-            for platform in platforms:
+            for platform in self.platforms:
                 if player.colliderect(platform.rect):
                     print("collide")
                     self.player.velocity = pygame.Vector2(0, 0)
