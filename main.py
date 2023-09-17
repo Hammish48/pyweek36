daf4e503239ec9a5661e5b3483e0e011b048e241
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-17 17:48:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from game import Game
 
 pygame.init()
 
@@ -7,23 +8,9 @@ def main():
     screen = pygame.display.set_mode((1120, 580))
     pygame.display.set_caption("caption")
     fps = pygame.time.Clock()
-    pen = pygame.draw
+    game = Game()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        # game logic
-
-
-        # rendering
-        screen.fill((0, 0, 0))
-
-        pen.rect(screen, (0, 200, 20), pygame.Rect(50, 50, 50,50))
-
-        fps.tick(60)
-        pygame.display.update()
+    game.run(screen, fps)
     
 
 if __name__ == "__main__":
