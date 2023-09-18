37b8ceff9b49390918dee1c4e2c67c97a56dfb6d
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-18 11:52:48 +1000

    Update Block.py

--- Block.py
+++ Block.py
@@ -3,11 +3,16 @@ import pygame
 # assets\top_block.png
 
 class Platform:
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, texture):
         self.position = pygame.Vector2(x, y)
         self.size = pygame.Vector2(width, height)
         self.hitbox = pygame.Rect(x, y, width, height)
-        self.color = color
+        self.texture = texture
     def render(self, camera, screen):
         self.rect = pygame.Rect(self.position.x - camera.target.x + camera.offset.x, self.position.y - camera.target.y + camera.offset.y, self.size.x, self.size.y)
-        pygame.draw.rect(screen, self.color, self.rect)
+        if self.texture == 1:
+            screen.blit(pygame.image.load("assets/grass block.png"), (self.position.x- camera.target.x + camera.offset.x, self.position.y- camera.target.y + camera.offset.y))
+        elif self.texture == 2:
+            screen.blit(pygame.image.load("assets/dirt block.png"), (self.position.x- camera.target.x + camera.offset.x, self.position.y- camera.target.y + camera.offset.y))
+        elif self.texture == 3:
+            pygame.draw.rect(screen, "red", self.rect)
