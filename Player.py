0b6d3adcf9f396583077d375795100b898fa879c
Reactoimpact <reactoimpact@hotmail.com>
2023-09-20 14:00:31 +1000

    Added simple gun inventory system

--- Player.py
+++ Player.py
@@ -21,9 +21,13 @@ class Player():
         self.gun_tip = 0
         self.infection = 0
         self.infection_rate = 0.01
+        self.gun = Gun(0, 10, 5, "blue", 2, 30, 1)
+        self.gun2 = Gun(0, 200, 300, "red", 10, 3, 10)
     
     def physicsProcess(self, platforms, enemies, camera, flyingEnemies, cures):
         self.infection += self.infection_rate
+        self.velocity.x = 0
+        
         if self.infection > 100:
             self.alive = False
 
@@ -35,9 +39,17 @@ class Player():
                 else:
                     self.infection = 0
 
+        # user Input
+        if pygame.mouse.get_pressed()[0] and self.gun.cooldown == 0:
+            self.gun.shoot(self.bullets)
+        if self.gun.cooldown > 0:
+            self.gun.cooldown -= 1  # Decrement the cooldown timer
+        if pygame.mouse.get_pressed()[2] and self.gun2.cooldown == 0:
+            self.gun2.shoot(self.bullets)
+        if self.gun2.cooldown > 0:
+            self.gun2.cooldown -= 1  # Decrement the cooldown timer
+    
 
-        
-        self.velocity.x = 0
         if pygame.key.get_pressed()[pygame.K_a]:
             self.velocity.x -= 5
             self.animation.tick()
@@ -94,57 +106,11 @@ class Player():
         if self.velocity.y > 50:
             self.alive = False
 
-        # Gun Phys - move to another class? ----------------------------------------
+        self.gun.physics(self.position, camera)
+        self.gun.check_bullet_collisions(self.bullets, platforms, enemies, flyingEnemies)
 
-        # get pos of hand on player
-        hand_pos = pygame.math.Vector2(
-            self.position.x - camera.target.x + camera.offset.x + self.size.x - self.size.x/2, 
-            self.position.y - camera.target.y + camera.offset.y + self.size.y-30
-        )
-
-        # get cursor pos
-        cursor = pygame.math.Vector2(pygame.mouse.get_pos())
-        direction = cursor - hand_pos
-
-        if direction.x != 0 or direction.y != 0:
-            direction.normalize_ip()
-        
-        if (self.forward and math.atan2(direction.x, direction.y) > 0) or (not self.forward and math.atan2(direction.x, direction.y) < 0):
-            self.angle = math.atan2(direction.x, direction.y)
-            self.end = hand_pos + direction * 30
-        else:
-            self.forward = not self.forward
-
-        self.gun_tip = hand_pos + direction * 30
-        angle = math.atan2(direction.x, direction.y)
-        # end
-
-        if pygame.mouse.get_pressed()[0] and self.shoot_cooldown == 0:
-            self.bullets.append(Bullet(self.position.x + direction.x, self.position.y + direction.y, angle))
-            self.shoot_cooldown = self.shoot_cooldown_duration
-        if self.shoot_cooldown > 0:
-            self.shoot_cooldown -= 1  # Decrement the cooldown timer
-
-        for index, bullet in enumerate(self.bullets):
-            if bullet.life < 0:
-                self.bullets.pop(index)
-            for platform in platforms:
-                if platform.hitbox.collidepoint(bullet.position):
-                    self.bullets.pop(index)
-            for index, enemy in enumerate(enemies):
-                if enemy.hitbox.collidepoint(bullet.position):
-                    enemy.health -= 1
-                    if enemy.health < 0:
-                        enemies.pop(index)
-                        print("ded enemy")
-            for index, enemy in enumerate(flyingEnemies):
-                if enemy.hitbox.collidepoint(bullet.position):
-                    print("\033[4m\033[92m\033[1mLIFE IS ROBLOC")
-                    enemy.health -= 1
-                    if enemy.health <= 0:
-                        flyingEnemies.pop(index)
-                        print("DJ KHALID WE THE BEST MUSICCCC")
-            bullet.move()
+        self.gun2.physics(self.position, camera)
+        self.gun2.check_bullet_collisions(self.bullets, platforms, enemies, flyingEnemies)
         
 
     def render(self, screen, platforms, camera, enemies):
@@ -160,28 +126,93 @@ class Player():
             else:
                 screen.blit(pygame.transform.flip(pygame.image.load("./assets/player/jump.png"), True, False), self.position - camera.target + camera.offset)
 
-        pygame.draw.line(screen, "black", (
-            self.position.x - camera.target.x + camera.offset.x + self.size.x - self.size.x/2, 
-            self.position.y - camera.target.y + camera.offset.y + self.size.y-30
-        ), self.gun_tip, 7)
+        self.gun.render(screen, camera)
+        self.gun2.render(screen, camera)
 
         for bullet in self.bullets:
-            bullet.draw(screen, camera)
+            bullet.render(screen, camera)
+
+
+class Gun:
+    def __init__(self, gun_type = 0, cooldown_time = 30, bullet_life = 60, color = "red", bullet_size = 5, bullet_speed = 25, bullet_damage = 1):
+        self.player_pos = pygame.Vector2(0, 0)
+        self.gun_type = gun_type
+        self.cursor_pos = pygame.mouse.get_pos()
+        self.gun_tip = (0, 0)
+        self.gun_angle = 0
+        self.cooldown = 30
+        self.cooldown_time = cooldown_time
+        self.bullet_life = bullet_life
+        self.color = color
+        self.bullet_size = bullet_size
+        self.bullet_speed = bullet_speed
+        self.bullet_damage = bullet_damage
+
+    def physics(self, player_pos, camera):
+        self.player_pos = player_pos
+        pos = pygame.math.Vector2(
+            player_pos.x - camera.target.x + camera.offset.x + 15, 
+            player_pos.y - camera.target.y + camera.offset.y + 20
+        )
+        cursor = pygame.mouse.get_pos()
+        direction = cursor - pos
+
+        if direction != (0, 0):
+            direction.normalize_ip()
+
+        self.gun_tip = pos + direction * 30
+        self.gun_angle = math.atan2(direction.x, direction.y)
+    
+    def check_bullet_collisions(self, bullets, platforms, enemies, flyingEnemies):
+        for index, bullet in enumerate(bullets):
+            if bullet.life < 0:
+                bullets.pop(index)
+            for platform in platforms:
+                if platform.hitbox.collidepoint(bullet.position):
+                    bullets.pop(index-1)
+            for index, enemy in enumerate(enemies):
+                if enemy.hitbox.collidepoint(bullet.position):
+                    enemy.health -= self.bullet_damage
+                    if enemy.health < 0:
+                        enemies.pop(index)
+                        print("ded enemy")
+            for index, enemy in enumerate(flyingEnemies):
+                if enemy.hitbox.collidepoint(bullet.position):
+                    enemy.health -= self.bullet_damage
+                    if enemy.health <= 0:
+                        flyingEnemies.pop(index)
+            bullet.move()
+
+    def shoot(self, bullets):
+        bullets.append(Bullet(self.player_pos.x, self.player_pos.y, self.gun_angle, self.bullet_life, self.bullet_speed, self.color, self.bullet_size))
+        self.cooldown = self.cooldown_time
+    
+
+    def render(self, screen, camera):
+        pygame.draw.line(screen, self.color, (
+            self.player_pos.x - camera.target.x + camera.offset.x + 15, 
+            self.player_pos.y - camera.target.y + camera.offset.y + 20
+        ), self.gun_tip , 7)
 
 
 class Bullet:
-    def __init__(self, x, y, angle):
+    def __init__(self, x = 0, y = 0, angle = 90, life = 60, speed = 25, color = "red", size = 5):
         self.position = pygame.Vector2(x + 15, y + 20)
         self.angle = angle
-        self.life = 60
+        self.life = life
+        self.speed = speed
+        self.color = color
+        self.size = size
     
     def move(self):
-        self.position.x += 25 * math.sin(self.angle)
-        self.position.y += 25 * math.cos(self.angle)
+        self.position.x += self.speed * math.sin(self.angle)
+        self.position.y += self.speed * math.cos(self.angle)
         self.life -= 1
     
-    def draw(self, screen, camera):
-        pygame.draw.circle(screen, "black", (
+    def render(self, screen, camera):
+        pygame.draw.circle(screen, self.color, (
             self.position.x - camera.target.x + camera.offset.x,
             self.position.y - camera.target.y + camera.offset.y)
-        , 5)
+        , self.size)
+
+
