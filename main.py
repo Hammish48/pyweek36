e65fa51d449d4a1cdd4ed141e0b54140aaf263c8
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-22 13:08:47 +1000

    insanely important change, game is dependent on it

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 
 def main():
     screen = pygame.display.set_mode((1120, 580))
-    pygame.display.set_caption("caption")
+    pygame.display.set_caption('I love the part where he said "its gaming time" and then gamed everywhere')
     fps = pygame.time.Clock()
     game = Game()
     startup = Startup()
