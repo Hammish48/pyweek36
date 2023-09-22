b2b44402c8ea42b684d7a602aa74d70e8877c117
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-22 18:50:35 +1000

    made textures load at the start so that they aren't being loaded every frame

--- Block.py
+++ Block.py
@@ -5,23 +5,23 @@ class Platform:
         self.position = pygame.Vector2(x, y)
         self.size = pygame.Vector2(width, height)
         self.hitbox = pygame.Rect(x, y, width, height)
-        self.texture = texture
-        self.darktexture = darktexture
+        self.texture = pygame.image.load("assets/" + texture + ".png").convert_alpha()
+        self.darktexture = pygame.image.load("assets/" + darktexture + ".png").convert_alpha()
         self.dark = False
     def render(self, camera, screen):
-        self.rect = pygame.Rect(
-            self.position.x - camera.target.x + camera.offset.x,
-            self.position.y - camera.target.y + camera.offset.y,
-            self.size.x,
-            self.size.y
-        )
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(
-            self.position.x - camera.target.x + camera.offset.x + 4,
-            self.position.y - camera.target.y + camera.offset.y + 4,
-            self.size.x,
-            self.size.y
-        ))
+        #self.rect = pygame.Rect(
+        #    self.position.x - camera.target.x + camera.offset.x,
+        #    self.position.y - camera.target.y + camera.offset.y,
+        #    self.size.x,
+        #    self.size.y
+        #)
+        #pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(
+        #    self.position.x - camera.target.x + camera.offset.x + 4,
+        #    self.position.y - camera.target.y + camera.offset.y + 4,
+        #    self.size.x,
+        #    self.size.y
+        #))
         if not self.dark:
-            screen.blit(pygame.image.load("assets/" + self.texture + ".png").convert_alpha(), self.rect)
+            screen.blit(self.texture, self.position - camera.target + camera.offset)
         else:
-            screen.blit(pygame.image.load("assets/" + self.darktexture + ".png").convert_alpha(), self.rect)
+            screen.blit(self.darktexture, self.position - camera.target + camera.offset)
