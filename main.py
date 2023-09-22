670a562b15b0b33a6d8a0b00eba0de0518b77ae4
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-22 20:36:58 +1000

    added icon

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 def main():
     screen = pygame.display.set_mode((1120, 580))
     pygame.display.set_caption('I love the part where he said "its gaming time" and then gamed everywhere')
+    pygame.display.set_icon(pygame.image.load("./assets/dark brick block.png"))
     fps = pygame.time.Clock()
     from game import Game
     game = Game()
