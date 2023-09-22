08a76eaa7021376e689d735d4708ae2660e8c83a
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-22 11:58:55 +1000

    more efficient rendering

--- Cure.py
+++ Cure.py
@@ -5,12 +5,8 @@ class Cure:
         self.position = pygame.Vector2(x, y)
         self.size = pygame.Vector2(50, 50)
         self.hitbox = pygame.Rect(x, y, 50, 50)
+        self.sprite = pygame.image.load("assets/cure.png").convert_alpha()
     def render(self, camera, screen):
-        self.rect = pygame.Rect(
-            self.position.x - camera.target.x + camera.offset.x,
-            self.position.y - camera.target.y + camera.offset.y,
-            self.size.x,
-            self.size.y
-        )
         
-        screen.blit(pygame.image.load("assets/cure.png").convert_alpha(), self.rect)
+        screen.blit(self.sprite, self.position - camera.target + camera.offset)
+
