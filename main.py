086cede6b0e1784080afad62cfd2db34b3b97737
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-23 13:13:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 pygame.init()
+pygame.mixer.init()
 
 def main():
+    pygame.mixer.music.load("assets/Space Fighter Loop.mp3")
+    pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.music.play(-1)
     screen = pygame.display.set_mode((1120, 580))
     pygame.display.set_caption('A dying world - pyweek 36')
     pygame.display.set_icon(pygame.image.load("./assets/dark brick block.png"))
