046d08dbf3a986f7204222e1c12aede8a50183f2
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-09-23 11:16:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import pygame
-import sys
-from startup import Startup
-
 pygame.init()
 
 def main():
     screen = pygame.display.set_mode((1120, 580))
-    pygame.display.set_caption('I love the part where he said "its gaming time" and then gamed everywhere')
+    pygame.display.set_caption('A dying world - pyweek 36')
     pygame.display.set_icon(pygame.image.load("./assets/dark brick block.png"))
     fps = pygame.time.Clock()
-    from game import Game
-    game = Game()
+    from startup import Startup
     startup = Startup()
 
     startup.run(screen, fps)
 
-    game.load_map("level_1")
-    game.run(screen, fps)
     
 
 if __name__ == "__main__":
