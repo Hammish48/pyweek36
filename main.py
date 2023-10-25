a9f5018fb6987564a2833e1d630cdd1613c50aef
Hammish48 <111051617+Hammish48@users.noreply.github.com>
2023-10-25 20:49:49 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
+from pygame.locals import *
 pygame.init()
 pygame.mixer.init()
-
+pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
 def main():
     pygame.mixer.music.load("assets/Space Fighter Loop.mp3")
     pygame.mixer.music.set_volume(0.2)
     pygame.mixer.music.play(-1)
     screen = pygame.display.set_mode((1120, 580))
     pygame.display.set_caption('A dying world - pyweek 36')
-    pygame.display.set_icon(pygame.image.load("./assets/dark brick block.png"))
+    pygame.display.set_icon(pygame.image.load("./assets/dark block.png").convert())
     fps = pygame.time.Clock()
     from startup import Startup
     startup = Startup()
@@ -17,5 +18,4 @@ def main():
 
     
 
-if __name__ == "__main__":
-    main()
+main()
